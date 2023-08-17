784b240c5164883aeef4811e0615210dcb903fca
sds7629 <sds7629@naver.com>
2023-08-17 21:34:47 +0900

    numnumnum

--- bigreviews/models.py
+++ bigreviews/models.py
@@ -3,11 +3,15 @@ from common.models import CommonModel
 
 
 class Bigreview(CommonModel):
-    author = models.ForeignKey(
-        "users.User", on_delete=models.CASCADE, related_name="rreviews"
+    author = models.ForeignKey(  # 대댓글 작성자
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="bigreviews_written",  # 작성자 입장에서 해당 작성자가 작성한 대댓글들을 가져올 때 사용할 이름
     )
-    parent_review = models.ForeignKey(
-        "reviews.Review", on_delete=models.CASCADE, related_name="rreviews"
+    parent_review = models.ForeignKey(  # 대댓글이 달린 원본 댓글
+        "reviews.Review",
+        on_delete=models.CASCADE,
+        related_name="bigreviews",  # 원본 댓글의 입장에서 해당 원본 댓글에 달린 대댓글들을 가져올 때 사용할 이름
     )
     content = models.CharField(max_length=140)
     is_block = models.BooleanField(default=False)
